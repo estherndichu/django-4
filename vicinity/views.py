0ebf4e8d2d884e2f6c9254d252d4e6fc632b5b70
estherndichu <itskuijenga@gmail.com>
2021-04-12 16:00:32 +0300

    neighborhood view and templates

--- vicinity/views.py
+++ vicinity/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Neighborhood, User, Business
+from .forms import NeighborhoodForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        form = NeighbourHoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.admin = request.user.profile
+            hood.save()
+            return redirect('index')
+    else:
+        form = NeighbourHoodForm()
+    return render(request, 'vicinity/index.html', {'form': form})
