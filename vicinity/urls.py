fb200b5ca6f41bb29a2f6e9aba9018a9c55387ae
estherndichu <itskuijenga@gmail.com>
2021-04-13 08:18:08 +0300

    edit vicinity url

--- vicinity/urls.py
+++ vicinity/urls.py
@@ -7,7 +7,7 @@ from . import views
 
 urlpatterns = [
     url('',views.index,name='index'),
-    url('^new_vicinity/',views.new_vicinity),
+    url('^new_vicinity/',views.new_vicinity, name='new_vicinity'),
     url("^profile/", views.profile, name="profile"),
     url('^update_profile/$',views.update_profile,name = 'update_profile'),
 
