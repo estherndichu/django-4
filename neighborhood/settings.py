85b312b6ff0feaf1d1175084458b42857498b148
estherndichu <itskuijenga@gmail.com>
2021-04-17 15:42:56 +0300

    bootstrap

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -41,10 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'vicinity',
     'crispy_forms',
+    'bootstrap4',
     'django_registration',
 ]
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
